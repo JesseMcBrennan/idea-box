cdd01b6d9b57784bea2101291be890534d1802fe
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-03-30 15:52:22 -0600

    Add delete functionality

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var objectToStore = JSON.stringify('.idea-list')
-var save = $('.save-button')
+// var objectToStore = JSON.stringify('.idea-list');
+// var json = '{"ideaTitle":true, "count":25}';
+// obj = JSON.parse(json);
+// console.log(json)
+var save = $('.save-button');
 var ideaNumber = 0
-
 
 save.click(storeIdea);
 
@@ -10,18 +12,37 @@ function storeIdea(event){
   var ideaTitle = $('#idea-title').val();
   var ideaBody = $('#idea-body').val();
   ideaNumber++
-  var ideaCard = 
+  var ideaCard =
     $('.idea-list').prepend(`<article id="${ideaNumber}">
       <h3 contenteditable="true">${ideaTitle}</h3>
-      <input type="image" aria-label="delete" class="delete-button" alt="delete" src="images/delete.svg">
+      <input type="button" aria-label="delete" class="delete-button" alt="delete">
       <p contenteditable="true">${ideaBody}</p>
-      <input type="image" aria-label="upvote" class="upvote-button" alt="upvote" src="images/upvote.svg">
-      <input type="image" aria-label="downvote" class="downvote-button" alt="downvote" src="images/downvote.svg">
+      <input type="button" aria-label="upvote" class="upvote-button" alt="upvote">
+      <input type="button" aria-label="downvote" class="downvote-button" alt="downvote">
       <p class="quality">quality:
       <span class="rating">swill</span>
       </p>      
-    </article>`)
-  $('#idea-title , #idea-body').val('')
-  }
+    </article>`);
+  $('#search').removeClass('search');
+  $('#idea-title , #idea-body').val('');
+}
 
 
+$('main').on('click', 'article .delete-button', deleteIdea);
+
+function deleteIdea(event) {
+  event.preventDefault();
+  $(this).parent('article').remove()
+};
+
+$('main').on('click', 'article .upvote-button', upvoteIdea); 
+
+function upvoteIdea(event) {
+  event.preventDefault();  
+};
+
+$('main').on('click', 'article .downvote-button', downvoteIdea)
+
+function downvoteIdea(event) {
+  event.preventDefault();
+}
