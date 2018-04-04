d01fafdfca247a86d02a5a125572cc8e693d232e
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-04-03 18:04:40 -0600

    Add upvote/downvote/edit functionality

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// var objectToStore = JSON.stringify('.idea-list');
-// var json = '{"ideaTitle":true, "count":25}';
-// obj = JSON.parse(json);
-// console.log(json)
-// getCard()
 
 var $save = $('.save-button');
-var cardArray = []
 
-// loadCards();
+loadCards();
 $save.click(storeIdea);
-
 
 
 function storeIdea(event) {
@@ -17,56 +10,51 @@ function storeIdea(event) {
   var $ideaTitle = $('#idea-title').val();
   var $ideaBody = $('#idea-body').val();
   var $ideaId = event.timeStamp;
-  var newCard = new CreateCard($ideaId, $ideaTitle, $ideaBody);
-  $newCard.prependCard();
+  var $quality = 'swill'
+  var newCard = new CreateCard($ideaId, $ideaTitle, $ideaBody, $quality);
+  prependCard(newCard.id, newCard.title, newCard.body, newCard.quality);
   storeCard($ideaId, newCard);
 }
 
-function CreateCard(id, title, body) {
+function CreateCard(id, title, body, quality) {
   this.id = id;
   this.title = title;
   this.body = body;
+  this.quality = quality;
 }
 
-CreateCard.prototype.prependCard = function() { 
-  $('.idea-list').prepend(`<article id="${this.id}">
-  <h3 contenteditable="true">${this.title}</h3>
+function prependCard(id, title, body, quality) { 
+  $('.idea-list').prepend(`<article id="${id}">
+  <h3 class="title content" contenteditable="true">${title}</h3>
   <input type="button" aria-label="delete" class="delete-button" alt="delete">
-  <p contenteditable="true">${this.body}</p>
+  <p class="body content" contenteditable="true">${body}</p>
   <input type="button" aria-label="upvote" class="upvote-button" alt="upvote">
   <input type="button" aria-label="downvote" class="downvote-button" alt="downvote">
   <p class="quality">quality:
-  <span class="rating">swill</span>
+  <span class="rating">${quality}</span>
   </p>      
   </article>`);
   $('#search').removeClass('search');
   $('#idea-title , #idea-body').val('');
 }
 
+function loadCards() {
+  for (var i = 0; i < localStorage.length; i++) { 
+    var localStorageid = localStorage.key(i)
+    var retrievedCard = localStorage.getItem(localStorageid);
+    var parsedCard = JSON.parse(retrievedCard);
+    prependCard(parsedCard.id, parsedCard.title, parsedCard.body, parsedCard.quality);
+  };
+}
+
+function getCard(id) {
+  var retrievedCard = localStorage.getItem(id);
+  var parsedCard = JSON.parse(retrievedCard);
+}
+
 function storeCard(id, card) {
   var stringCard = JSON.stringify(card);
   localStorage.setItem(id, stringCard);
-}
-
-// function loadCards() {
-
-//   for (var i = 0; i < localStorage.length; i++) {
-
-
-
-//     getCard("id", "card")
-//     prependCard("id", "title", "body")
-//     // for each, getCard() & parse if needed
-//    // prepend all to page
-//   }
-// }
-
-
-
-function getCard(id, card) {
-  var retrievedCard = localStorage.getItem(id)
-  JSON.parse(card);
-  console.log('test')
 }
 
 $('#search').on('keyup', search);
@@ -75,33 +63,59 @@ function search() {
   var input = $('search').val()
 }
 
-
 $('main').on('click', 'article .delete-button', deleteIdea);
 
 function deleteIdea(event) {
   event.preventDefault();
-  localStorage.removeItem(this.id)
-  $(this).parent('article').remove()
+  localStorage.removeItem($(this).parent('article').attr('id'));
+  $(this).parent('article').remove() 
 };
+
+
+$('main').on('blur', 'article .content', editContent); 
+
+function editContent() {
+  var $id = $(this).parent('article').attr('id')
+  var retrievedCard = localStorage.getItem($id);
+  var parsedCard = JSON.parse(retrievedCard);
+  console.log($(this).parent('article').children('.title').val())
+  parsedCard.title = $(this).parent('article').children('.title').text();
+  parsedCard.body = $(this).parent('article').children('.body').text();
+  storeCard($id, parsedCard)
+}
 
 $('main').on('click', 'article .upvote-button', upvoteIdea); 
 
 function upvoteIdea(event) {
   event.preventDefault();
+  var $id = $(this).parent('article').attr('id');
+  var retrievedCard = localStorage.getItem($id);
+  var parsedCard = JSON.parse(retrievedCard);
   var $rating = $('.rating');
   if ($(this).siblings('p').children($rating).text() === 'swill') {
-    $(this).siblings('p').children($rating).text('plausible');
+    $(this).siblings('p').children($rating).text('plausible')
+    parsedCard.quality = 'plausible';
+    return storeCard($id, parsedCard)
   } else if ($(this).siblings('p').children($rating).text() === 'plausible') 
-    $(this).siblings('p').children($rating).text('genius');
+    $(this).siblings('p').children($rating).text('genius')
+    parsedCard.quality = 'genius'; 
+    storeCard($id, parsedCard)
 };
 
 $('main').on('click', 'article .downvote-button', downvoteIdea)
 
 function downvoteIdea(event) {
   event.preventDefault();
+  var $id = $(this).parent('article').attr('id');
+  var retrievedCard = localStorage.getItem($id);
+  var parsedCard = JSON.parse(retrievedCard);
   var $rating = $('.rating');
   if ($(this).siblings('p').children($rating).text() === 'genius') {
     $(this).siblings('p').children($rating).text('plausible');
+    parsedCard.quality = 'plausible';
+    return storeCard($id, parsedCard)
   } else if ($(this).siblings('p').children($rating).text() === 'plausible') 
     $(this).siblings('p').children($rating).text('swill');
+    parsedCard.quality = 'swill';  
+    storeCard($id, parsedCard)
 };
